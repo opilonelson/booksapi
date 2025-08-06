f1ccf32c376d268377a2d10b668172f766c019c8
opilonelson <30505192+opilonelson@users.noreply.github.com>
2025-08-07 02:42:21 +0300

    Change User to Book

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
-
 
 # Use environment variable
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://flaskuser:secret@localhost/flaskapi")
@@ -12,50 +12,21 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class User(db.Model):
+# Model: Book
+class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    title = db.Column(db.String(200), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    isbn = db.Column(db.String(20), unique=True, nullable=False)
 
     def to_dict(self):
-        return {"id": self.id, "name": self.name, "email": self.email}
+        return {"id": self.id, "title": self.title, "author": self.author, "isbn": self.isbn}
 
 with app.app_context():
     db.create_all()
 
-@app.route('/users', methods=['POST'])
-def create_user():
-    data = request.json
-    user = User(name=data['name'], email=data['email'])
-    db.session.add(user)
-    db.session.commit()
-    return jsonify(user.to_dict()), 201
-
-@app.route('/users', methods=['GET'])
-def get_users():
-    users = User.query.all()
-    return jsonify([u.to_dict() for u in users])
-
-@app.route('/users/<int:user_id>', methods=['GET'])
-def get_user(user_id):
-    user = User.query.get_or_404(user_id)
-    return jsonify(user.to_dict())
-
-@app.route('/users/<int:user_id>', methods=['PUT'])
-def update_user(user_id):
-    user = User.query.get_or_404(user_id)
-    data = request.json
-    user.name = data.get('name', user.name)
-    user.email = data.get('email', user.email)
-    db.session.commit()
-    return jsonify(user.to_dict())
-
-@app.route('/users/<int:user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    user = User.query.get_or_404(user_id)
-    db.session.delete(user)
-    db.session.commit()
-    return jsonify({"message": "User deleted"})
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+# Create a new book
+@app.route('/books', methods=['POST'])
+def create_book():
+    data = request.get_json()
+    if not data or not data.get('title') or not data.get
